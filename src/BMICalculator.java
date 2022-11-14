ff0f8f9ee306372959aa0e88b0760c45c348698c
itruitt2027 <itruitt2027@gmail.com>
2022-11-14 10:55:29 -0500

    Added return statementss

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -1,4 +1,13 @@
 public class BMICalculator {
+    /** Convert English to metric units, perform the BMI calculation.
+     * NOTE:this method myst properly handle bad data
+     * @param inches user's height in inches
+     * @param pounds user's weight in pounds
+     * @return User's BMI weight(kg)/height(m)^2 a double
+     */
+
+    public static double computeBMI(int inches, int pounds) {
+        return 0.0;
 
     }
 }
